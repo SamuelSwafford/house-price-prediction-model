45b1fae4bd7c1ad520905d44776401335ff739e0
mvh267 <148588398+mvh267@users.noreply.github.com>
2024-04-21 21:02:37 -0500

    Created SQL file

---
+++ data_sqldb.sql
@@ -0,0 +1,26 @@
+SELECT * FROM "Data";
+
+DROP TABLE "Data";
+
+CREATE TABLE "Data" (
+    "Num" TEXT,
+    "Listing_ID" TEXT,
+    "St" TEXT,
+    "MLS_Area" TEXT,
+    "Address" TEXT,
+    "NumBeds" INTEGER,
+    "NumFull_Baths" INTEGER,
+    "NumHalf_Baths" INTEGER,
+    "NumGarage_Spaces" INTEGER,
+    "Levels" TEXT,
+    "Year_Built" INTEGER,
+    "Acres" REAL,
+    "SqFt" INTEGER,
+    "List_Price" TEXT,
+    "LPDollar_per_SqFt" TEXT,
+    "CloseDollar_per_SqFt" TEXT,
+    "Close_Price" TEXT,
+    "Close_Date" TEXT,
+    "DOM" INTEGER,
+    "CDOM" INTEGER
+);
